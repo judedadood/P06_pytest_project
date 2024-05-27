887126612521c1874c8c31c2481f039dfed132f6
jude <23021096@myrp.edu.sg>
2024-05-27 14:04:57 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,55 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_subtract(self):
+        # arrange
+        a = 5
+        b = 3
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 2
+        assert result == expected
+
+    def test_multiply(self):
+        # arrange
+        a = 5
+        b = 3
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 15
+        assert result == expected
+
+
+    def test_divide(self):
+        # arrange
+        a = 15
+        b = 3
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 5
+        assert result == expected
